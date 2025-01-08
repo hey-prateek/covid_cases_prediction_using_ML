54980e3396c9b8bf857754f4f839342234b8cf5d
prateek <71164915+hey-prateek@users.noreply.github.com>
2025-01-08 11:07:15 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+
+<html>
+    <head><meta charset="utf-8" /></head>
+    <body>
+        <center><h1>Prediction of COVID-2019 rise and fall with an improved prediction model</h1><br>
+            <h2>(Multi-peak Weibull Model)</h2>
+           
+<?php
+$myfile = fopen("plots/lastupdated.txt", "r");
+echo fgets($myfile);
+fclose($myfile);
+?> IST</b>. </i><br>
+
+<iframe width="700" height="500" frameborder="0" scrolling="no" src="plots/World_pred.html"></iframe>
+<iframe width="700" height="500" frameborder="0" scrolling="no" src="plots/World_total.html"></iframe>
+
+<hr>
+
+<iframe width="700" height="500" frameborder="0" scrolling="no" src="plots/India_pred.html"></iframe>
+<iframe width="700" height="500" frameborder="0" scrolling="no" src="plots/India_total.html"></iframe>
+
+<hr>
+
+<iframe width="700" height="500" frameborder="0" scrolling="no" src="plots/United States_pred.html"></iframe>
+<iframe width="700" height="500" frameborder="0" scrolling="no" src="plots/United States_total.html"></iframe>
+
+<hr>
+
+<iframe width="700" height="500" frameborder="0" scrolling="no" src="plots/United Kingdom_pred.html"></iframe>
+<iframe width="700" height="500" frameborder="0" scrolling="no" src="plots/United Kingdom_total.html"></iframe>
+
+<hr>
+
+<iframe width="700" height="500" frameborder="0" scrolling="no" src="plots/Brazil_pred.html"></iframe>
+<iframe width="700" height="500" frameborder="0" scrolling="no" src="plots/Brazil_total.html"></iframe>
+
+<hr>
+
+<iframe width="700" height="500" frameborder="0" scrolling="no" src="plots/Italy_pred.html"></iframe>
+<iframe width="700" height="500" frameborder="0" scrolling="no" src="plots/Italy_total.html"></iframe>
+
+<hr>
+
+<iframe width="700" height="500" frameborder="0" scrolling="no" src="plots/France_pred.html"></iframe>
+<iframe width="700" height="500" frameborder="0" scrolling="no" src="plots/France_total.html"></iframe>
+
+<hr>
+
+<iframe width="700" height="500" frameborder="0" scrolling="no" src="plots/Russia_pred.html"></iframe>
+<iframe width="700" height="500" frameborder="0" scrolling="no" src="plots/Russia_total.html"></iframe>
+
+<hr>
+
+</center>
+    </body>
+</html>
